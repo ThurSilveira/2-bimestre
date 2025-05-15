29217998ea10964e77ab1b9fdbcd5c026ebfe0e2
ThurSilveira <arthursilveira13000@gmail.com>
2025-05-15 20:40:34 -0300

    5 corrigido

--- Exercício 5/js/scrips.js
+++ Exercício 5/js/scrips.js
@@ -1,3 +1,18 @@
 function ex5(){
-    
+    let preco = 5.0, quant = 120
+    const despesa = 200
+    let aux = ""
+    let lucro, maiorlucro = 0
+
+    while(preco >= 1.0){
+        lucro = (preco * quant) - despesa
+        if (lucro > maiorlucro){
+            maiorlucro = lucro
+        }
+        aux = aux + `\n` + (`${preco} - ${quant} - ${despesa} - ${lucro}`)
+        quant = quant + 26
+        preco = preco - 0.5
+    }
+    alert(aux)
+    alert(`O maior lucro é: ${maiorlucro}`)
 }
