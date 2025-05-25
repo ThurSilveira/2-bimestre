0965584df23b71e9f19614114119ceb69bb37a5a
ThurSilveira <arthursilveira13000@gmail.com>
2025-05-24 22:35:39 -0300

    6k

--- Exercício 6/js/scrips.js
+++ Exercício 6/js/scrips.js
@@ -8,4 +8,13 @@ function ex6(){
         }
         console.log(`Tabuada do ${num}:\n` + i )     
     }
-}           
+} 
+
+function tabuada(){
+    let numero = Number(prompt("Digite um número:"))
+    
+    for (let i = 1; i <= 10; i++){
+        console.log(`${numero} x ${i} = ${numero * i}`)
+    }
+
+}
