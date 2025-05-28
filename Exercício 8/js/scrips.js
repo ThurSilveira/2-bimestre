28df381e66eac22c93208df80bcf9bc327935fca
ThurSilveira <arthursilveira13000@gmail.com>
2025-05-27 21:58:15 -0300

    ex 8

--- Exercício 8/js/scrips.js
+++ Exercício 8/js/scrips.js
@@ -1,5 +1,5 @@
 function js(){
-    let idade, altura, peso, idademaior = 0, altura12 = 0, pesoinf = 0
+    let idade, altura, peso, idademaior = 0, pessoas12 = 0, altura12 = 0, pesoinf = 0
     for (let i = 0; i < 5; i++){
         idade = Number(prompt("Informe sua idade"))
         altura = Number(prompt("Informe sua altura")) 
@@ -8,12 +8,15 @@ function js(){
             idademaior++
         }
         else if (idade > 9 && idade < 21){
-             altura += altura12
+            pessoas12++
+            altura12 += altura
         }
         else if (peso < 40){
             pesoinf++
         }
 
-        
     }
+    alert(`O total de pessoas com idade superior a 50 anos:  ${idademaior}`)
+    alert(`A média das alturas das pessoas com idade entre 10 e 20 anos: ${altura12/pessoas12}`)
+    alert(`A porcentagem de pessoas com peso inferior a 40kg das demais: ${pesoinf * 100 / 5}%`)
 }
