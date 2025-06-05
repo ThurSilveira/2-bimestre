7e00f0b5ab0f19c78ab089ba0d2d5f4cb21117a2
ThurSilveira <arthursilveira13000@gmail.com>
2025-06-05 19:49:43 -0300

    Ex 2

--- VETORES/Exercício Vetor 2/js/scrips.js
+++ VETORES/Exercício Vetor 2/js/scrips.js
@@ -1,6 +1,20 @@
 function js(){
-    let par = []
-    let impar = []
+    let vet = []
 
-    // calcular e exibir 6 num ; numeros de pares e a quantidade; impar tbm
+    for(let i = 0; i < 6; i++){
+        vet.push(Number(prompt(`Informe o ${i+1}° elemento`))) // vet push = colocar valor no vet
+    }
+    //encontrar pares e impares
+    let pares = []
+    let impares = []
+    for(let i = 0; i < 6; i++){
+        if(vet[i] % 2 == 0){
+            pares.push(vet[i])// da valor ao vet par
+        }
+        else{
+            impares.push(vet[i])
+        }
+    }
+    alert(`Quantidade de pares ${pares.length} - ${pares}`)
+    alert(`Quantidade de impares ${impares.length} - ${impares}`) // Lenght = tamanho 
 }
